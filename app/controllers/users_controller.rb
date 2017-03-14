a019e35033003ab85318da709deb2bf1ffcafc66
NguyenMinhTuan2906 <monteeladykillah@gmail.com>
2017-03-14 09:35:48 +0700

    Finish user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new,:create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :load_user, except: [:new, :index, :create]
+
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.paginate.per_page
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
+  def edit
   end
 
   def create
@@ -18,9 +30,53 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-    def user_params
-      params.require(:user).permit :name, :email, :password,
-        :password_confirmation
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render :edit
     end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".delete"
+    else
+      flash[:danger] = t ".delete_fail"
+    end
+    redirect_to users_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".log_in"
+      redirect_to login_url
+    end
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def correct_user
+    load_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = t ".none"
+      redirect_to root_url
+    end
+  end
 end
